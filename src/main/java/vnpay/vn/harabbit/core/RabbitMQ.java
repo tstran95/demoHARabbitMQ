79d4e5bae0430ef95331873259cfb3db9cd1f8ea
sontt1 <tstran95>
2022-10-03 16:34:15 +0700

    ref Producer

--- src/main/java/vnpay/vn/harabbit/core/RabbitMQ.java
+++ src/main/java/vnpay/vn/harabbit/core/RabbitMQ.java
@@ -43,13 +43,17 @@ public class RabbitMQ {
      */
     public boolean openConnection() {
         try {
-            Address[] address = {new Address("localhost" , 5_000)};
+            Address[] address = {new Address("172.19.0.5" , 5_000) ,
+                                new Address("172.19.0.3" , 5_000),
+                                new Address("172.19.0.2" , 5_000)};
             factory = new ConnectionFactory();
+//            factory.setPort(5000);
+//            factory.setHost("localhost");
             factory.setUsername("sontt");
             factory.setPassword("sontt");
             factory.setAutomaticRecoveryEnabled(true);
             factory.setRequestedHeartbeat(45);
-            factory.setConnectionTimeout(60_000);
+            factory.setConnectionTimeout(60000);
             connection = factory.newConnection(address);
             log.info("Connect success to RabbitMQ ");
             return true;
